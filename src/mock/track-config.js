69bd265f95a87869e9400f6a0de4176d97bc2898
hughfenghen <hughfenghen@gmail.com>
2017-10-16 19:26:43 +0800

    获取xpath 函数

--- src/mock/track-config.js
+++ src/mock/track-config.js
@@ -1,9 +1,12 @@
-export default [{
-  name: '点击header',
-  type: 'click',
-  nodePath: 'html body header',
-}, {
-  name: '看见footer',
-  type: 'view',
-  nodePath: 'html body footer',
-}]
+export default [
+  {
+    name: '点击header',
+    type: 'click',
+    nodePath: 'html body header'
+  },
+  {
+    name: '看见footer',
+    type: 'view',
+    nodePath: 'html body footer'
+  }
+]
--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,10 @@ export function getNodePath (el) {
     return node.tagName === 'HTML' ? ps : getAllParent(node.parentNode, ps)
   }
 
-  return getAllParent(el, []).reverse().map(tagName => tagName.toLowerCase()).join(' ')
+  return getAllParent(el, [])
+    .reverse()
+    .map(tagName => tagName.toLowerCase())
+    .join(' ')
 }
 
 /**
@@ -15,4 +18,28 @@ export function htmlToElement (html) {
   var template = document.createElement('template')
   template.innerHTML = html
   return template.content.firstChild
+}
+
+/**
+ * 获取节点的XPath  XPath由id、标签名、索引构成
+ * @param  {Element} element element
+ * @return {string}         element XPath表达式
+ */
+export function getElementXPath (element) {
+  if (!element) return null
+
+  if (element.id) {
+    return '//*[@id="' + element.id + '"]'
+  } else if (element.tagName === 'BODY') {
+    return '/html/body'
+  } else {
+    const sameTagSiblings = Array.from(element.parentNode.childNodes)
+      .filter(e => e.nodeName === element.nodeName)
+    const idx = sameTagSiblings.indexOf(element)
+
+    return getElementXPath(element.parentNode) +
+      '/' +
+      element.tagName.toLowerCase() +
+      (sameTagSiblings.length > 1 ? `[${idx + 1}]` : '')
+  }
 }
